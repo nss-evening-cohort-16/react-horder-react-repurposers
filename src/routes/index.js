5d50e0fea3228e5bc83d1acdb900c2518f25d7fe
Elle Lawrence <lawrence.danielle@gmail.com>
2021-10-25 21:20:30 -0500

    setting up routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,17 @@
-// index for router
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../views/Home';
+import MyStuff from '../views/MyStuff';
+import NewStuff from '../views/NewStuff';
+
+export default function Routes() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/stuff" component={MyStuff} />
+        <Route exact path="/new" component={NewStuff} />
+      </Switch>
+    </div>
+  );
+}
