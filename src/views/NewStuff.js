7064c21819920732c08b2d0154f2b70cbc9ac58b
Elle Lawrence <lawrence.danielle@gmail.com>
2021-10-26 19:33:33 -0500

    new stuff view

--- src/views/NewStuff.js
+++ src/views/NewStuff.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Form } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-export default function NewStuff() {
-  return <div>FORM</div>;
+export default function NewStuff({
+  stuffObj, setStuff, setEditStuff, user,
+}) {
+  return (
+    <div>
+      <Form
+        stuffObj={stuffObj}
+        setStuff={setStuff}
+        setEditStuff={setEditStuff}
+        user={user}
+      />
+    </div>
+  );
 }
+
+NewStuff.propTypes = {
+  stuffObj: PropTypes.shape({
+    itemName: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    itemImage: PropTypes.string,
+    itemDescription: PropTypes.string,
+    uid: PropTypes.string,
+  }),
+  setStuff: PropTypes.func.isRequired,
+  setEditStuff: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    uid: PropTypes.string,
+  }),
+};
+
+NewStuff.defaultProps = { stuffObj: {}, user: null };
