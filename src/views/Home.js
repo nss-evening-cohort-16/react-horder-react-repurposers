6b395276f679a91a1ad9d179436839da7590f425
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-10-29 20:55:39 -0500

    added background images to both Home and SignIn pages

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import img from '../images/pageBackgroundImage.png';
+
+const Content = styled.div`
+  border: 1px solid #000;
+  background-image: url(${img});
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+`;
+
+const MidText = styled.p`
+  text-align: center;
+  font-weight: bold;
+  margin-top: 40px;
+`;
 
 export default function Home() {
   return (
-    <div>
-      <h1>HOME</h1>;
-    </div>
+    <Content>
+      <div>
+        <Header>React Repurposers</Header>
+        <div>
+          <MidText>Repurposing the items that you do not want.</MidText>
+        </div>
+      </div>
+    </Content>
   );
 }
