ae50fab3f623a2a63871f6dc9894552b18d7968b
Craig Wellspring <wellspring.craig@gmail.com>
2021-10-30 14:57:50 -0500

    polaroid styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,33 +9,33 @@ const FormContainer = styled.div`
   width: 60%;
   margin: auto;
   padding: 50px 0;
-  
+
   h1 {
     color: #444430;
     text-align: center;
     font-size: 84px;
     font-weight: 400;
     font-family: 'Heebo', sans-serif;
-    text-shadow: 2px 2px #A9A29E;
+    text-shadow: 2px 2px #a9a29e;
   }
 
-  h5{
+  h5 {
     text-align: center;
     font-size: 35px;
-    color: #A56A26;
+    color: #a56a26;
     font-family: 'Nothing You Could Do', cursive;
   }
 
-  input{
+  input {
     font-size: 18px;
-    color: #A56A26;
+    color: #a56a26;
     font-family: 'Nothing You Could Do', cursive;
   }
 
-  button{
+  button {
     font-family: 'Heebo', sans-serif;
-    background-color: #E0CCAA
-    outline-color: #A56A26;
+    background-color: #e0ccaa;
+    outline-color: #a56a26;
   }
 `;
 
--- src/views/SingleStuff.js
+++ src/views/SingleStuff.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import ReactCardFlip from 'react-card-flip';
 import { deleteStuff, getSingleStuff } from '../api/data/stuffData';
 import backgroundImage from '../images/pageBackgroundImage.png';
 
 const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   height: 100%;
   width: 100%;
@@ -12,69 +16,103 @@ const Background = styled.div`
 `;
 
 const PolaroidFrame = styled.div`
+  background-color: white;
+
   display: flex;
   flex-direction: column;
   row-gap: 10px;
   align-items: center;
   text-align: center;
 
-  padding: 10px;
+  width: 3.5in;
+  height: 4.2in;
+  padding: 20px;
+  border: 1px solid black;
   margin: 10px;
 `;
 
+const FrameFront = styled.div``;
+
+const FrameBack = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Photo = styled.img`
-  max-width: 500px;
-  max-height: 500px;
+  width: 3.1in;
+  height: 3.1in;
+  border: 1px solid black;
 `;
 
 const Caption = styled.div`
+  margin-top: 0.3em;
+  align-items: center;
   font-family: 'Nothing You Could Do', cursive;
+  font-size: 150%;
+  font-weight: bold;
   color: #444340;
 `;
 
 const Description = styled.div`
-  max-width: 500px;
+  font-family: 'Nothing You Could Do', cursive;
+  color: #444340;
 `;
 
 const ButtonContainer = styled.div`
+  height: 100%;
+  align-items: flex-end;
   display: flex;
   column-gap: 10px;
   justify-content: center;
 `;
 
 export default function SingleStuff() {
+  const history = useHistory();
   const [singleStuff, setSingleStuff] = useState({});
   const { key } = useParams();
-  const history = useHistory();
 
   useEffect(() => {
     getSingleStuff(key).then(setSingleStuff);
   }, []);
 
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <Background>
-      <PolaroidFrame>
-        <Photo src={singleStuff.itemImage} alt="ItemImage" />
-        <Caption>{singleStuff.itemName}</Caption>
-        <Description>{singleStuff.itemDescription}</Description>
-        <ButtonContainer>
-          <Link
-            to={`/edit/${singleStuff.firebaseKey}`}
-            className="btn btn-primary"
-          >
-            Edit
-          </Link>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={() => {
-              deleteStuff(singleStuff.firebaseKey).then(() => history.push('/stuff'));
-            }}
-          >
-            Delete
-          </button>
-        </ButtonContainer>
-      </PolaroidFrame>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <PolaroidFrame onClick={handleClick}>
+          <FrameFront>
+            <Photo src={singleStuff.itemImage} alt="ItemImage" />
+            <Caption>{singleStuff.itemName}</Caption>
+          </FrameFront>
+        </PolaroidFrame>
+        <PolaroidFrame onClick={handleClick}>
+          <FrameBack>
+            <Description>{singleStuff.itemDescription}</Description>
+            <ButtonContainer>
+              <Link
+                to={`/edit/${singleStuff.firebaseKey}`}
+                className="btn btn-outline-secondary"
+              >
+                <i className="fas fa-paperclip" />
+              </Link>
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                onClick={() => {
+                  deleteStuff(singleStuff.firebaseKey).then(() => history.push('/stuff'));
+                }}
+              >
+                <i className="fas fa-trash-alt" />
+              </button>
+            </ButtonContainer>
+          </FrameBack>
+        </PolaroidFrame>
+      </ReactCardFlip>
     </Background>
   );
 }
